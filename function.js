8fa0b7dd60aa08506f8644ddd4a20e308abf4c8e
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-11-10 15:07:48 +0530

    function

---
+++ function.js
@@ -0,0 +1,33 @@
+document.write("addition"+"<br>")
+document.write("------------"+"<br>")
+function add(a,b){
+  var c=a+b;
+    return c;
+}
+document.write(add(50,20)+"<br>");
+
+document.write("subtraction"+"<br>")
+document.write("------------"+"<br>")
+function sup(x,y){
+    var z=x-y;
+    return z;
+}
+document.write(sup(50,20)+"<br>");
+
+document.write("multiplication"+"<br>")
+document.write("------------"+"<br>")
+function multi(z,y){
+    var x=z*y;
+    return x;
+}
+document.write(multi(100,100)+"<br>")
+
+document.write("division"+"<br>")
+document.write("------------"+"<br>")
+function div(b,a){
+   var c=b/a;
+   return c;
+}
+document.write(div(50,5)+"<br>")
+var a="10"+20+30;
+document.write(a)
